002c4b9aa75493c4efbc88e8ce146d16fb6a7bb4
mihirkadam19 <mihirkadam85@yahoo.in>
2024-09-28 20:19:46 -0400

    gemini

--- Django/resumeUpload/upload/views.py
+++ Django/resumeUpload/upload/views.py
@@ -30,6 +30,20 @@ def extract_text_from_file(file):
         return extract_text_from_docx(BytesIO(file.read()))
     else:
         raise ValueError("Unsupported file format")
+    
+def get_skills(prompt, text):
+  prompt = prompt + text
+  return model.generate_content(prompt)
+
+def custom_prompt(prompt, skills, job_title, job_description):
+  return prompt + "\n" + "Resume Skills: " + skills + "\n" + "Job Title: " + job_title + "\n" + "Job Description: " + job_description
+
+def gemini(text,job_title, job_description):
+    prompt_1 = "From the given text extract all the skills and just given the mentioned skills in the text as output and nothing else. Also if the skills are repeated then give only one don't repeat them. Text: "
+    skills = get_skills(prompt_1, text)
+    prompt_2 = "Given a resume, job title and a job description, extract and compare the required skills from both documents. First, identify all the skills mentioned in the job description, including technical, soft, and domain-specific skills. Then, extract all the skills from the resume. Compare the two sets of skills and identify the top 5 missing or underdeveloped skills in the resume that are crucial for the job described. Return these 5 skills along with a brief explanation of why they are necessary for the role."
+    prompt = custom_prompt(prompt_2, skills.text, job_title, job_description)
+    return model.generate_content(prompt).text
 
 def home(request):
     if request.method == 'POST':
@@ -44,11 +58,10 @@ def home(request):
 
                 # Process the uploaded file directly from request.FILES
                 extracted_text = extract_text_from_file(resume_file)
-
+                msg = gemini(extracted_text,designation_job,description_job)
                 # Print the inputs (for demonstration)
                 #print(f'Name: {name}, Email: {email}, Job: {goal_job}, Resume: {extracted_text}')
 
-                msg = ''
                 return render(request, 'index.html', {
                     'msg': msg
                 })
